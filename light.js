7d4801a71778359090172e5abcb0a09756261f4d
abcnam514 <147784898+abcnam514@users.noreply.github.com>
2024-12-12 06:45:57 +0900

    Update light.js

--- light.js
+++ light.js
@@ -8,7 +8,7 @@ radio.forEach(item => {
         if(item.value === 'light') {
 
             background.style.background = 'linear-gradient(180deg, rgba(252,252,252,1) 0%, rgba(143,143,143,1) 73%, rgba(99,99,99,1) 100%)'
-            work.style.background = 'url("img/work.png") no-repeat'
+            work.style.background = 'url("img/Work.png") no-repeat'
 
         }else if(item.value === 'dark') {
             background.style.background = 'linear-gradient(180deg, rgba(36,36,36,1) 0%, rgba(101,101,101,1) 27%, rgba(252,252,252,1) 100%)'
